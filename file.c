378f08507ea1de5762f71cac36f92b1a9588874a
aquintero10 <andresquintero10@outlook.es>
2018-02-28 15:25:04 -0500

    GeneraArchivo

---
+++ file.c
@@ -0,0 +1,17 @@
+#include < stdio.h >
+#include < stdlib.h >
+
+int main() {
+    int i;
+    FILE *fp;
+
+    fp=fopen("bigfakefile.txt","w");
+
+    for(i=0;i<(1024*1024);i++) {
+        fseek(fp,(1024*1024), SEEK_CUR);
+        fprintf(fp,"C");
+    }
+
+    fclose(fp);
+    return 0;
+}
