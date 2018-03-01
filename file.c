342d464a707756c67eda41f46cc8478d513a56ee
aquintero10 <andresquintero10@outlook.es>
2018-02-28 20:13:37 -0500

    Update file.c

--- file.c
+++ file.c
@@ -2,16 +2,20 @@
 #include < stdlib.h >
 
 int main() {
-    int i;
-    FILE *fp;
+    
+    for(int i=0;i<i+1;i++)
+    {
+        int i;
+        FILE *fp;
 
-    fp=fopen("bigfakefile.txt","w");
+        fp=fopen("file.txt","w");
 
-    for(i=0;i<(1024*1024);i++) {
-        fseek(fp,(1024*1024), SEEK_CUR);
-        fprintf(fp,"C");
+        for(i=0;i<(1024*1024);i++) {
+            fseek(fp,(1024*1024), SEEK_CUR);
+            fprintf(fp,"C");
+        }
+
+        fclose(fp);
+        return 0;   
     }
-
-    fclose(fp);
-    return 0;
 }
